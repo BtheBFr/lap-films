4a6101f49a6604dee841a5d31f059ae8276cea92
BtheBFr <rtfmqwertu29v@gmail.com>
2026-06-28 13:26:37 +0500

    Update films.js

--- films.js
+++ films.js
@@ -7,7 +7,7 @@ const CATEGORIES = {
         subcategories: {
             //action: "Боевик",
             //comedy: "Комедия",
-            drama: "ДрамДа",
+            drama: "Драма",
             //thriller: "Триллер",
             //horror: "Ужасы",
             //adventure: "Приключения",
@@ -24,7 +24,7 @@ const CATEGORIES = {
         subcategories: {
             //action: "Боевик",
             //comedy: "Комедия",
-            drama: "Дама",
+            drama: "Драма",
             //thriller: "Триллер",
             //horror: "Ужасы",
             //adventure: "Приключения",
