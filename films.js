9705b5d0d9aeb952dc077dfd9550cd7b6b6b1f14
BtheBFr <rtfmqwertu29v@gmail.com>
2026-07-20 11:57:25 +0500

    Update films.js

--- films.js
+++ films.js
@@ -88,16 +88,16 @@ const FILMS_CATALOG = [
         "1": {
             title: "Сезон 1",
             series: {
-                "1": { title: "Серия 1", videoUrl: "https://nuno.ru/embed/jDT1JnSxW2a?autoplay=1" },
+                "1": { title: "Серия 1", videoUrl: "hhttps://kinescope.io/embed/3BirZY6zGJHa6cZjtbHPrV?autoplay=1" },
                 "2": { title: "Серия 2", videoUrl: "https://kinescope.io/embed/e4EYUzRqpT9ppvxsbPhVqR?autoplay=1" },
                 "3": { title: "Серия 3", videoUrl: "https://kinescope.io/embed/hEQZCq74ivS3gC2imUNBve?autoplay=1" },
                 "4": { title: "Серия 4", videoUrl: "https://kinescope.io/embed/kiWzsuj1PH31eKX9iFSAfD?autoplay=1" },
                 "5": { title: "Серия 5", videoUrl: "https://kinescope.io/embed/wvUDso7wXYTHavYcGwwYym?autoplay=1" },
                 "6": { title: "Серия 6", videoUrl: "https://kinescope.io/embed/rCPCATm7GoKM3o9aqtnr82?autoplay=1" },
                 "7": { title: "Серия 7", videoUrl: "https://kinescope.io/embed/2npU4FADoEWmdP8xiSE66a?autoplay=1" },
-                "8": { title: "Серия 8", videoUrl: "https://nuno.ru/embed/bBMsOg5HgIi?autoplay=1" },
+                "8": { title: "Серия 8", videoUrl: "https://kinescope.io/embed/aps5Jj5pwpuWA1fyiL3mcP?autoplay=1" },
                 "9": { title: "Серия 9", videoUrl: "https://nuno.ru/embed/0HQOpLimmjg?autoplay=1" },
-                "10": { title: "Серия 10", videoUrl: "https://nuno.ru/embed/bhIkE07gxHo?autoplay=1" }
+                "10": { title: "Серия 10", videoUrl: "https://kinescope.io/embed/vbGRqCWZNUwnDm57Y6caTy?autoplay=1" }
             }
         }
     },
